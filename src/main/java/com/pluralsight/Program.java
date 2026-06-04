e7e5b951e00aa2d0302674a5ce25409ea4d7731b
jolienguyen0831 <uyennguyen0831@gmail.com>
2026-06-04 14:59:51 -0500

    Complete jcdb processing

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,4 +1,52 @@
 package com.pluralsight;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.Instant;
+
 public class Program {
+
+    public static void main(String[] args) {
+
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl("jdbc:mysql://localhost:3306/sakila");
+        dataSource.setUsername("root");
+        dataSource.setPassword("yearup26");
+
+        String sql = """
+                SELECT actor_id,
+                    first_name,
+                    last_name,
+                    last_update
+                FROM actor;
+                """;
+
+        try (Connection connection = dataSource.getConnection()) {
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+
+                int actorID = resultSet.getInt("actor_id");
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                Instant lastUpdate = resultSet.getTimestamp("last_update").toInstant();
+
+                System.out.printf("%d, %s, %s, %s\n", actorID, firstName, lastName, lastUpdate.toString());
+
+            }
+
+            resultSet.close();
+            statement.close();
+            connection.close();
+
+        } catch (SQLException e) {
+            System.out.println("Cant find database");
+            e.printStackTrace();
+        }
+    }
 }
